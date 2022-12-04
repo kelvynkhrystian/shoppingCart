428462e2c920dfaeaf5d96833d089e3309eccf50
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-04 03:09:41 -0300

    add-func-getProducts

--- src/script.js
+++ src/script.js
@@ -1,8 +1,22 @@
-const btnCart = document.getElementById('rigth-header');
-const aside = document.getElementById('cart-box');
-const article = document.getElementById('produts-box');
-  
-btnCart.addEventListener('click', () => {
-  aside.classList.toggle("oit");
-  article.classList.toggle("quin");
-}); 
+// Menu retrátil
+const menu = () => {
+  const btnCart = document.getElementById('rigth-header');
+  const aside = document.getElementById('cart-box');
+  const article = document.getElementById('produts-box');
+    
+  btnCart.addEventListener('click', () => {
+    aside.classList.toggle("oit");
+    article.classList.toggle("quin");
+  });
+}
+
+menu();
+
+const getProducts = async (computador) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${computador}`
+  const result = await fetch(url);
+  const data = await result.json();
+  console.log(data);
+};
+
+getProducts('computador');
